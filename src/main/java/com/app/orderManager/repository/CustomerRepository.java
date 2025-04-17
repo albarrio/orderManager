5ed1713c3bf032b4d7f52017a6859086cb52ee55
alberto.barrionuevo <alberto.barrionuevo@training4u.it>
2025-04-17 15:20:02 +0200

    customer repo

---
+++ src/main/java/com/app/orderManager/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.app.orderManager.repository;
+
+import com.app.orderManager.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
