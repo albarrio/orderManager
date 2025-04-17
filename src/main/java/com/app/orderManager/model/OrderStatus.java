931152f8e9ed4e14453713e8e0b24138474b3724
alberto.barrionuevo <alberto.barrionuevo@training4u.it>
2025-04-17 15:21:05 +0200

    order repo and status

---
+++ src/main/java/com/app/orderManager/model/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.app.orderManager.model;
+
+public enum OrderStatus {
+    PENDING,    // Ordine in attesa
+    IN_PROGRESS, // Ordine in lavorazione
+    COMPLETED   // Ordine completato
+}
---
+++ src/main/java/com/app/orderManager/service/OrderService.java
@@ -0,0 +1,35 @@
+package com.app.orderManager.service;
+
+import com.app.orderManager.model.Order;
+import com.app.orderManager.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(int id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public Order updateOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
